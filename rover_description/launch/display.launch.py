ef03157aab897f511a70973222ae7fa0f5d781fc
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-06-02 17:16:19 +0600

    launch file python

---
+++ rover_description/launch/display.launch.py
@@ -0,0 +1,37 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.parameter_descriptions import ParameterValue
+
+def generate_launch_description():
+    urdf_path = PathJoinSubstitution([
+        FindPackageShare('rover_description'),
+        'urdf',
+        'urdf_wheel.urdf'
+    ])
+
+    robot_description = ParameterValue(
+        Command([FindExecutable(name='cat'), ' ', urdf_path]),
+        value_type=str  # Tell ROS it's a string
+    )
+
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            parameters=[{'robot_description': robot_description}]
+        ),
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui'
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen'
+        )
+    ])
